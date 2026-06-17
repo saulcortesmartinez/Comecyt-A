2a062495139bd3d541e7ee7bc7aaa5b2e18c8974
saul martinez <saulmartinez.02000@gmail.com>
2026-06-16 19:10:51 -0600

    Corrigiendo ruta de imagen en modulo_2_contenido_5

--- src/pages/contenido/modulo_2_contenido_5.jsx
+++ src/pages/contenido/modulo_2_contenido_5.jsx
@@ -4,11 +4,6 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../Css/modulo_2_contenido_5.css";
 
-import Publicacion_fbImg from "../../assets/Publicacion_fb.png";
-import Seguridad_fbImg from "../../assets/Seguridad_fb.png";
-import Seguridad2_fbImg from "../../assets/Seguridad2_fb.png";
-import RecuperarCon_fbImg from "../../assets/RecuperarCon_fb.png";
-import Paso2_fbImg from "../../assets/Paso2_fb.png";
 
 const API_URL = "http://localhost:4000";
 const MODULO_ID = 2; // ✅ Es módulo 2
