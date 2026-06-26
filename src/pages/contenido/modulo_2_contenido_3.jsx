5b1ab8f82c75608ab91a20fda762852e9ed4f500
saulcortesmartinez <saulmartinez.02000@gmail.com>
2026-06-26 12:19:44 -0600

    Update modulo_2_contenido_3.jsx

--- src/pages/contenido/modulo_2_contenido_3.jsx
+++ src/pages/contenido/modulo_2_contenido_3.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../Css/modulo_2_contenido_3.css";
-import publicacionImg from "../../assets/publicacion_fb.png";
+
 import gruposImg from "../../assets/grupos_fb.png";
 import paginasImg from "../../assets/paginas_fb.png";
 import eventosImg from "../../assets/eventos_fb.png";
